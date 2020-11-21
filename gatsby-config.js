b9d852829cdcc2d8349a47dcecf8b0da5548e6db
YSkuo <crowley3141@hotmail.com>
2020-11-22 00:05:31 +0800

    update deploy setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ require(`dotenv`).config()
 const { toSlug } = require("@reflexjs/gatsby-helpers") 
 
 module.exports = {
+  pathPrefix: "/site",
   siteMetadata: {
     title: "Arsene Kuo",
     description: "Arsene's Personal Website.",
